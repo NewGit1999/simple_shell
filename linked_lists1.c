0a1ad848b6b843504230084dacfff4419e747cb0
NewGit1999 <yasminebenali32@gmail.com>
2023-02-22 11:24:15 -0800

    update linked_lists1.c

--- linked_lists1.c
+++ linked_lists1.c
@@ -3,7 +3,7 @@
 /**
  * add_node - adds a node to the start of the list
  * @head: address of pointer to head node
- * @str: str field of node
+ * @str: str field of node.
  * @num: node index used by history
  *
  * Return: size of list
