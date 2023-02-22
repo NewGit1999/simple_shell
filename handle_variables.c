445f32f31d24024e5dbc63c229e3bb7d298166fb
NewGit1999 <yasminebenali32@gmail.com>
2023-02-22 11:46:39 -0800

    updates to handle argument variables

--- handle_variables.c
+++ handle_variables.c
@@ -3,7 +3,7 @@
 /**
  * is_chain - test if current char in buffer is a chain delimeter
  * @info: the parameter struct
- * @buf: the char buffer
+ * @buf: the char buffer.
  * @p: address of current position in buf
  *
  * Return: 1 if chain delimeter, 0 otherwise
