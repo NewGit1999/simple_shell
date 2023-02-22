9ffd419fad5733cc12181baf507a4b21e75904f4
NewGit1999 <yasminebenali32@gmail.com>
2023-02-22 11:22:00 -0800

    update handle_memory.c

--- handle_memory.c
+++ handle_memory.c
@@ -3,7 +3,7 @@
 /**
  * *_memset - fills memory with a constant byte
  * @s: the pointer to the memory area
- * @b: the byte to fill *s with
+ * @b: the byte to fill *s with.
  * @n: the amount of bytes to be filled
  * Return: (s) a pointer to the memory area s
  */
